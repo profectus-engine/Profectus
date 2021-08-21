00ddf767dae84c7571807c26846475ca4218bae0
thepaperpilot <thepaperpilot@gmail.com>
2021-08-20 21:13:31 -0500

    Added replit support

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,11 @@ module.exports = {
             .plugin("tsconfig-paths")
             // eslint-disable-next-line @typescript-eslint/no-var-requires
             .use(require("tsconfig-paths-webpack-plugin"));
+        // Remove this if/when all "core" code has no non-ignored more type errors
+        // https://github.com/vuejs/vue-cli/issues/3157#issuecomment-657090338
+        config.plugins.delete('fork-ts-checker');
+    },
+    devServer: {
+        disableHostCheck: true
     }
 };
