322ad1529a8d4a128b53959fcc3c43fdf90a12ae
thepaperpilot <thepaperpilot@gmail.com>
2022-03-03 22:28:37 -0600

    Removed unused parameter from exponential scaling function

--- src/features/conversion.ts
+++ src/features/conversion.ts
@@ -134,7 +134,6 @@ export function createLinearScaling(
 // e.g. if exponent is 0.5 and base is 10, then having 10 points makes gain 1, and 40 points is 2
 export function createExponentialScaling(
     base: DecimalSource | Ref<DecimalSource>,
-    coefficient: DecimalSource | Ref<DecimalSource>,
     exponent: DecimalSource | Ref<DecimalSource>
 ): ScalingFunction {
     return {
