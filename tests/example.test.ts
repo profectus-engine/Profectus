56bd7324e11174eee165a608666b0b5fe5babf41
thepaperpilot <thepaperpilot@gmail.com>
2022-06-23 11:31:25 -0500

    Write tests for common.ts

--- tests/example.test.ts
+++
@@ -1,6 +0,0 @@
-import Decimal from "util/bignum";
-
-test("Decimals?", () => {
-    const x = new Decimal(3);
-    expect(x.m).toBe(3);
-});
---
+++ tests/util/common.test.ts
@@ -0,0 +1,25 @@
+import { jest, describe, expect, test } from "@jest/globals";
+import { camelToTitle, isFunction } from "util/common";
+
+describe("camelToTitle", () => {
+    test("Capitalizes first letter in single word", () =>
+        expect(camelToTitle("test")).toBe("Test"));
+
+    test("Converts three word camel case string to title case", () =>
+        expect(camelToTitle("camelCaseTest")).toBe("Camel Case Test"));
+});
+
+describe("isFunction", () => {
+    test("Given function returns true", () => expect(isFunction(jest.fn())).toBe(true));
+
+    // Go through all primitives and basic types
+    test("Given a string returns false", () => expect(isFunction("test")).toBe(false));
+    test("Given a number returns false", () => expect(isFunction(10)).toBe(false));
+    test("Given a bigint returns false", () => expect(isFunction(BigInt(10))).toBe(false));
+    test("Given a boolean returns false", () => expect(isFunction(true)).toBe(false));
+    test("Given undefined returns false", () => expect(isFunction(undefined)).toBe(false));
+    test("Given a symbol returns false", () => expect(isFunction(Symbol())).toBe(false));
+    test("Given null returns false", () => expect(isFunction(null)).toBe(false));
+    test("Given object returns false", () => expect(isFunction({})).toBe(false));
+    test("Given array returns false", () => expect(isFunction([])).toBe(false));
+});
