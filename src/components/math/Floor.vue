b6d5eaa72d7486be1b7b3c6b220bd3b7cceedff9
thepaperpilot <thepaperpilot@gmail.com>
2022-06-26 23:03:40 -0500

    Added math components folder

---
+++ src/components/math/Floor.vue
@@ -0,0 +1,5 @@
+<template>
+    <span style="white-space: nowrap; font-size: larger; font-family: initial">
+        ⌊&nbsp;<slot />&nbsp;⌋
+    </span>
+</template>
---
+++ src/components/math/Sqrt.vue
@@ -0,0 +1,5 @@
+<template>
+    <span style="white-space: nowrap;">
+        <span style="font-size: larger; font-family: initial">&radic;</span><span style="text-decoration: overline">&nbsp;<slot />&nbsp;</span>
+    </span>
+</template>
