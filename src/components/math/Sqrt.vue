64d6aae1c014f44dd2d55716b6f7eaec6ca66876
thepaperpilot <thepaperpilot@gmail.com>
2022-06-28 09:53:07 -0500

    Reformat Sqrt.vue

--- src/components/math/Sqrt.vue
+++ src/components/math/Sqrt.vue
@@ -1,5 +1,6 @@
 <template>
-    <span style="white-space: nowrap;">
-        <span style="font-size: larger; font-family: initial">&radic;</span><span style="text-decoration: overline">&nbsp;<slot />&nbsp;</span>
+    <span style="white-space: nowrap">
+        <span style="font-size: larger; font-family: initial">&radic;</span
+        ><span style="text-decoration: overline"><slot /></span>
     </span>
 </template>
