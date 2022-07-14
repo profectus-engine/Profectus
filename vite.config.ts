92c47beafa1c462c683f91be147aa307b7ac2237
thepaperpilot <thepaperpilot@gmail.com>
2022-07-13 23:57:11 -0500

    Update vite config for better deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,18 @@ import projInfo from "./src/data/projInfo.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "./",
+    build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                  if (id.includes('node_modules')) {
+                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                  }
+              }
+            }
+        }
+    },
     plugins: [
         vue(),
         vueJsx({
