2af202a7f16c20c208800041797498ce0ced598d
thepaperpilot <thepaperpilot@gmail.com>
2022-07-26 15:37:38 -0500

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,18 +10,18 @@ export default defineConfig({
     base: "./",
     build: {
         rollupOptions: {
-            output:{
+            output: {
                 manualChunks(id) {
-                  if (id.includes('node_modules')) {
-                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                  }
-              }
+                    if (id.includes("node_modules")) {
+                        return id.toString().split("node_modules/")[1].split("/")[0].toString();
+                    }
+                }
             }
         }
     },
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js'
+            vue: "vue/dist/vue.esm-bundler.js"
         }
     },
     plugins: [
