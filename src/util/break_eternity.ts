19cb22bfd1560a5b17fbf5e8d06c93249b2d362b
jakub791 <84931788+jakub791@users.noreply.github.com>
2022-08-28 16:05:18 +0200

    test

--- src/util/break_eternity.ts
+++ src/util/break_eternity.ts
@@ -5,17 +5,20 @@ import Decimal from "lib/break_eternity";
 export default Decimal;
 
 const decimalOne = new Decimal(1);
-// yyyyy
+
+const e4 = new Decimal(1e4);
+const e9 = new Decima(1e4);
+
 export function exponentialFormat(num: DecimalSource, precision: number, mantissa = true): string {
     let e = Decimal.log10(num).floor();
     let m = Decimal.div(num, Decimal.pow(10, e));
     if (m.toStringWithDecimalPlaces(precision) === "10") {
-        m = decimalOne;
-        e = e.add(1);
+        m = decimal.dOne;
+        e = e.add(Decimal.dOne);
     }
-    const eString = e.gte(1e9)
-        ? format(e, Math.max(Math.max(precision, 3), projInfo.defaultDecimalsShown))
-        : e.gte(10000)
+    const eString = e.gte(e9)
+        ? format(e, Math.max(precision, 3, projInfo.defaultDecimalsShown))
+        : e.gte(e4)
         ? commaFormat(e, 0)
         : e.toStringWithDecimalPlaces(0);
     if (mantissa) {
@@ -26,31 +29,26 @@ export function exponentialFormat(num: DecimalSource, precision: number, mantiss
 }
 
 export function commaFormat(num: DecimalSource, precision: number): string {
-    if (num === null || num === undefined) {
-        return "NaN";
-    }
-    num = new Decimal(num);
+    num = Decimal.fromValue_noAlloc(num);
     if (num.mag < 0.001) {
         return (0).toFixed(precision);
     }
-    const init = num.toStringWithDecimalPlaces(precision);
-    const portions = init.split(".");
+    const portions = num.toStringWithDecimalPlaces(precision)
+    .split(".");
     portions[0] = portions[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     if (portions.length == 1) return portions[0];
     return portions[0] + "." + portions[1];
 }
 
 export function regularFormat(num: DecimalSource, precision: number): string {
-    if (num === null || num === undefined) {
-        return "NaN";
-    }
-    num = new Decimal(num);
+    num = Decimal.fronValue_noAlloc(num);
     if (num.mag < 0.0001) {
         return (0).toFixed(precision);
     }
     if (num.mag < 0.1 && precision !== 0) {
         precision = Math.max(
-            Math.max(precision, num.log10().negate().ceil().toNumber()),
+            precision, 
+            num.log10().negate().ceil().toNumber()),
             projInfo.defaultDecimalsShown
         );
     }
@@ -60,23 +58,23 @@ export function regularFormat(num: DecimalSource, precision: number): string {
 const eeee1000 = new Decimal("eeee1000");
 const e100000 = new Decimal("e100000");
 const e1000 = new Decimal("e1000");
-const e9 = new Decimal(1e9);
 const e6 = new Decimal(1e6);
 const e3 = new Decimal(1e3);
 const nearOne = new Decimal(0.98);
 const thousandth = new Decimal(0.001);
-const zero = new Decimal(0);
-export function format(num: DecimalSource, precision?: number, small?: boolean): string {
-    if (precision == null) precision = projInfo.defaultDecimalsShown;
-    small = small ?? projInfo.defaultShowSmall;
+
+export function format(
+    num: DecimalSource, 
+    precision = projInfo.defaultDecimalShown, 
+    small = projInfo.defaultShowSmal): string {
     num = new Decimal(num);
-    if (isNaN(num.sign) || isNaN(num.layer) || isNaN(num.mag)) {
+    if (Number.isNaN(num.mag)) {
         return "NaN";
     }
     if (num.sign < 0) {
         return "-" + format(num.neg(), precision);
     }
-    if (num.mag === Number.POSITIVE_INFINITY) {
+    if (num.mag === Infinity) {
         return "Infinity";
     }
     if (num.gte(eeee1000)) {
@@ -85,7 +83,7 @@ export function format(num: DecimalSource, precision?: number, small?: boolean):
             return "F" + format(slog.floor());
         } else {
             return (
-                Decimal.pow(10, slog.sub(slog.floor())).toStringWithDecimalPlaces(3) +
+                Decimal.dTen.pow(slog.sub(slog.floor())).toStringWithDecimalPlaces(3) +
                 "F" +
                 commaFormat(slog.floor(), 0)
             );
@@ -106,15 +104,15 @@ export function format(num: DecimalSource, precision?: number, small?: boolean):
 
     num = invertOOM(num);
     if (num.lt(e1000)) {
-        const val = exponentialFormat(num, precision);
-        return val.replace(/([^(?:e|F)]*)$/, "-$1");
+        return exponentialFormat(num, precision)
+        .replace(/([^(?:e|F)]*)$/, "-$1");
     } else {
         return format(num, precision) + "⁻¹";
     }
 }
 
 export function formatWhole(num: DecimalSource): string {
-    num = new Decimal(num);
+    num = Decimal.fromValue_noAlloc(num);
     if (num.sign < 0) {
         return "-" + formatWhole(num.neg());
     }
@@ -135,7 +133,7 @@ export function formatTime(seconds: DecimalSource): string {
         // integer precision limit
         return format(Decimal.div(seconds, 31536000)) + "y";
     }
-    seconds = new Decimal(seconds).toNumber();
+    seconds = Decimal.fromValue_noAlloc(seconds).toNumber();
     if (seconds < 60) {
         return format(seconds) + "s";
     } else if (seconds < 3600) {
@@ -171,7 +169,7 @@ export function formatTime(seconds: DecimalSource): string {
 }
 
 export function toPlaces(x: DecimalSource, precision: number, maxAccepted: DecimalSource): string {
-    x = new Decimal(x);
+    x = Decimal.fromValue_noAlloc(x);
     let result = x.toStringWithDecimalPlaces(precision);
     if (new Decimal(result).gte(maxAccepted)) {
         result = Decimal.sub(maxAccepted, Math.pow(0.1, precision)).toStringWithDecimalPlaces(
@@ -187,10 +185,7 @@ export function formatSmall(x: DecimalSource, precision?: number): string {
 }
 
 export function invertOOM(x: DecimalSource): Decimal {
-    let e = Decimal.log10(x).ceil();
-    const m = Decimal.div(x, Decimal.pow(10, e));
-    e = e.neg();
-    x = new Decimal(10).pow(e).times(m);
-
-    return x;
+    x = Decimal.fromValue_noAlloc(x);
+    const e = x.log10().ceil();
+    return Decimal.dTen.pow(e.neg()).times(x.div(Decimal.dTen.pow(e)));
 }
