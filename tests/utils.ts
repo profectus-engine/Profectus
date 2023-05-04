6540546432e77618dac81c5405f0087190fecb55
jakub791 <jakubkarolmazu@gmail.com>
2023-05-04 13:15:36 +0200

    fix augmentation of vitest Assertion interface
    The `Vi` namespace no longer exists since vitest 0.31.0.
    Using module augmentation is the new way.

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,18 +1,15 @@
 import Decimal, { DecimalSource, format } from "util/bignum";
-import { expect } from "vitest";
+import { expect, Assertion } from "vitest";
 
 interface CustomMatchers<R = unknown> {
     compare_tolerance(expected: DecimalSource, tolerance?: number): R;
 }
 
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace Vi {
-        // eslint-disable-next-line @typescript-eslint/no-empty-interface
-        interface Assertion extends CustomMatchers {}
-        // eslint-disable-next-line @typescript-eslint/no-empty-interface
-        interface AsymmetricMatchersContaining extends CustomMatchers {}
-    }
+declare module "vitest" {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface Assertion extends CustomMatchers {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
 
 expect.extend({
