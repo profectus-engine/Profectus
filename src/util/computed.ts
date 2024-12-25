0a5f63ff048227e74ad95e9e23ab41a6646c7d62
thepaperpilot <thepaperpilot@gmail.com>
2024-12-25 11:13:29 -0600

    Don't convert functions with parameters

--- src/util/computed.ts
+++ src/util/computed.ts
@@ -5,7 +5,7 @@ import { computed } from "vue";
 export type MaybeGetter<T> = T | (() => T);
 
 export function processGetter<T>(obj: T): T extends () => infer S ? ComputedRef<S> : T {
-    if (isFunction(obj)) {
+    if (isFunction(obj) && obj.length === 0) {
         return computed(obj) as ReturnType<typeof processGetter<T>>;
     }
     return obj as ReturnType<typeof processGetter<T>>;
